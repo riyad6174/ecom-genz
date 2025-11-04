7fbfea045d41e97a5afb3bfeb95158d5068e8071
Riyad <alimranhosen21@gmail.com>
2025-11-05 00:33:14 +0600

    added 20w

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -16,6 +16,7 @@ export default function Document() {
         />
 
         <link rel='preconnect' href='https://fonts.gstatic.com' crossorigin />
+
         <link
           href='https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@0,100..900;1,100..900&display=swap'
           rel='stylesheet'
